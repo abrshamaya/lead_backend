7bcaeeb2f73ea5a8393e95eaaa4239a80a8ef49a
Eual11 <uchihaeual11@gmail.com>
2025-10-31 18:13:29 +0300

    update settings

--- AmayaLead/AmayaLead/settings.py
+++ AmayaLead/AmayaLead/settings.py
@@ -97,26 +97,25 @@ CORS_ALLOWED_ORIGINS = [
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
-
-
 # DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": os.getenv("POSTGRES_DB", "lead_db"),
-#         "USER": os.getenv("POSTGRES_USER", "lead_user"),
-#         "PASSWORD": os.getenv("POSTGRES_PASSWORD", "lead_pass"),
-#         "HOST": os.getenv("POSTGRES_HOST", "db"),
-#         "PORT": os.getenv("POSTGRES_PORT", "5432"),
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_DB", "lead_db"),
+        "USER": os.getenv("POSTGRES_USER", "lead_user"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD", "lead_pass"),
+        "HOST": os.getenv("POSTGRES_HOST", "db"),
+        "PORT": os.getenv("POSTGRES_PORT", "5432"),
+    }
+}
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
@@ -171,6 +170,7 @@ Q_CLUSTER = {
     'retry': 1000000000,
     'max_attempts': 0,
     'workers': 8,
+    'retries': 0,
     'recycle': 500,
     'compress': True,
     'sync': False,
